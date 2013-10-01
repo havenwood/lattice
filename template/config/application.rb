458646f9984bf8f3c568680c8e66f9d1f7391cc1
Tony Arcieri <tony.arcieri@gmail.com>
2013-10-01 00:17:09 -0700

    Add a default Resources::Home resource

--- template/config/application.rb
+++ template/config/application.rb
@@ -1,5 +1,8 @@
 require File.expand_path("../boot", __FILE__)
 
+# Require your resources here
+require 'resources/home'
+
 module <%= app_const_base %>
   Application = Lattice::Application.new do |app|
     app.routes do
